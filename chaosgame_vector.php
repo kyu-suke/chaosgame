b40f6113500b066d8faea9e138172d3a9aac668e
kyu-suke <ykaneoka7@gmail.com>
2018-11-15 16:24:58 +0900

    addVector

--- chaosgame_vector.php
+++ chaosgame_vector.php
@@ -21,26 +21,27 @@ imagecolorallocate($img, 255, 255, 255);
 
 echo 'get points'.PHP_EOL;
 
-$pixels = [];
+$pixels = new \ds\vector();
 $last_point = ['x' => $width / 2, 'y' => $height / 2];
 foreach (range(0, $iterations) as $i) {
     $chosen_vertex = $vertex_points[mt_rand(0, count($vertex_points) - 1)];
     $last_point = ['x' => ($last_point['x'] + $chosen_vertex[0]) * $factor, 'y' => ($last_point['y'] + $chosen_vertex[1]) * $factor];
-    $pixels[] = $last_point;
+    $pixels->push($last_point);
 }
 
 echo 'prot points'.PHP_EOL;
 
 $color = imagecolorallocate($img, 0, 0, 0);
-foreach ($pixels as $v) {
+$pixels->map(function($v) use ($img, $color) {
     imagefilledellipse($img, $v['x'], $v['y'], 1, 1, $color);
-}
+});
 
-imagepng($img, 'out.png');
+imagepng($img, 'out_vector.png');
 echo 'end'.PHP_EOL;
 
 $time = microtime(true) - $time_start;
 echo ' microtime: '.$time.PHP_EOL;
 echo ' memory: '.memory_get_peak_usage(true).PHP_EOL;
 exit;
+
 
